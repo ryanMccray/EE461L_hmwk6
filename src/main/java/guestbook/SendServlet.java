3f1aa3cdcb222dffceb7f6c256309c441845a32f
Ken <knkobayashi@gmail.com>
2019-03-04 10:43:38 -0600

    Implemented Cron Job

--- src/main/java/guestbook/SendServlet.java
+++ src/main/java/guestbook/SendServlet.java
@@ -2,7 +2,9 @@
 package guestbook;
  
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,20 +12,41 @@ import javax.servlet.http.HttpServletResponse;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 import com.googlecode.objectify.*;
 import javax.servlet.http.*;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 
 public class SendServlet extends HttpServlet {
 	
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     	
+    	Properties props = new Properties();
+    	Session session = Session.getDefaultInstance(props, null);
+
     	ObjectifyService.register(Subscribe.class);
     	List<Subscribe> subscribers = ObjectifyService.ofy().load().type(Subscribe.class).list();   
-    	
-    	for(Subscribe subscriber: subscribers){
-    		String message = "Don't forget to check up on Bear Blog";
-    		//send message here
-    		subscriber.getEmailAddress();
-        }
+
+    	try {
+    		Message msg = new MimeMessage(session);
+    		msg.setFrom(new InternetAddress("admin@example.com", "Example.com Admin"));
+    		for(Subscribe subscriber: subscribers){
+    			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(subscriber.getEmailAddress(), "Bear Blog User"));
+    		}
+    		msg.setSubject("Bear Blog Reminder");
+    		msg.setText("Don't forget to check out Bear Blog!");
+    		Transport.send(msg);
+    	} catch (AddressException e) {
+    		// ...
+    	} catch (MessagingException e) {
+    		// ...
+    	} catch (UnsupportedEncodingException e) {
+    		// ...
+    	}
     
     }
 
